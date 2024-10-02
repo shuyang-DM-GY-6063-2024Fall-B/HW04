86af0c4511ecbbfec9409f9eca7aa5a90777a51f
TreeAspen <sy4254@nyu.edu>
2024-10-01 23:44:47 -0400

    finish（hope）

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,110 @@
+
+let x = 0;
+let speed = 5;
+let length = 10;
+let flowerDetails = [];
+//define all the colors I will use
+
+
+
+
 function setup() {
+  let color1 = '#4a7ded';
+  let color2 = '#6c54e8';
+  let color3 = '#2dc0d6';
+  let color4 = '#4139db';// dark blue
+  let color5 = '#fad43c';// yellow
+  let color6 = '#e8a438';// dark yellow 
+  let color7 = '#5151cf';
+  let color8 = '#3a40e8';
+  let color9 = '#b6e072';
+  let color10 = '#9f93fa';
+  let color11 = '#e887ac';
+  let color12 = '#b06f4c';
+  
+
+
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES); 
+  frameRate(30);
+  for (let a = 0; a < windowWidth; a += 8 * length) {
+    for (let b = 0; b < windowHeight; b += 8 * length) {
+      let num = floor(random(8, 12));
+      let d = a / 8 / length % 2 + 1;
+      let e = b / length / 8 % 2 + 1;
+      let direction = random([-1, 1])
+      let chosenc1 = random([color1,color2,color3,color10])
+      let chosenc2 = random([color4,color5,color6,color11])
+      let chosenc3 = random([color7,color8,color9,color12])
+      flowerDetails.push({ a, b, num, d, e, direction,chosenc1,chosenc2,chosenc3});
+    }
+  }
 }
 
+
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+  let deltaX = speed * deltaTime / 1000;
+
+  background('#0e1361');
+  // use the function to draw a flower
+  function flower(color,x,y,length,angle,n){
+    push();
+    noStroke();
+    // stroke(color2);
+    // strokeWeight(1);
+    fill(color);
+
+    translate(x, y);
+    rotate(angle);
+    // draw the single petal and stamen,and use for-loop to create the whole flower.
+    // I try to make stamens move with mouse be it seems stiff and I don't kown how to improve it.
+    for(let a = 0;a < 360 ;a += 360/n){
+      push();
+      rotate(a);
+      beginShape();
+      vertex(0, 0);
+      vertex(length*Math.sin(120/n* (Math.PI / 180)),length*Math.cos(120/n* (Math.PI / 180)));
+      vertex(1.5*length*Math.sin(60/n* (Math.PI / 180)),1.5*length*Math.cos(60/n* (Math.PI / 180))); 
+      vertex(0,length );
+      vertex(-1.5*length*Math.sin(60/n* (Math.PI / 180)),1.5*length*Math.cos(60/n* (Math.PI / 180)));
+      vertex(-length*Math.sin(120/n* (Math.PI / 180)),length*Math.cos(120/n* (Math.PI / 180)));
+      vertex(0, 0);
+      endShape();
+      // fill(color2);
+      // triangle(0,0,1.25*length*Math.cos(360 / n),1.25*length*Math.sin(360 / n),0.8*length*Math.cos(360 / 2*n),0.8*length*Math.sin(360 / 2*n));
+      // triangle(0,0,1.25*length*Math.cos(360 / n),-1.25*length*Math.sin(360 / n),0.8*length*Math.cos(360 / 2*n),-0.8*length*Math.sin(360 / 2*n));
+      // stroke(color2);
+      // strokeWeight(0.75);
+      // line(0,0,-1.5*length+(mouseX-windowWidth/2)/windowWidth*2*length,1.5*length+(mouseY-windowHeight/2)/windowHeight*2*length);
+      // ellipse(-1.5*length+(mouseX-windowWidth/2)/windowWidth*2*length,1.5*length+(mouseY-windowHeight/2)/windowHeight*2*length, length/3);
+      // stroke(color1);
+      // fill(color1);
+
+      pop();
+    }
+    pop();
+  }
+
+  x += deltaX;
+
+  // draw flower grids
+  for (let i = 0; i < flowerDetails.length; i++) {
+    let { a, b, num, d, e,direction,chosenc1,chosenc2,chosenc3} = flowerDetails[i];
+
+    if ((e + d) % 2 === 1) {
+      flower(chosenc3,  a-num*direction, b+2*num*direction, 6+num + Math.sin((x - num)/ 6)*num, -x *num/3*direction, num-4);
+      flower(chosenc2,  a+2*num*direction, b-num*direction, 4+num + Math.sin((x + num)/ 6)*num, x *num/3*direction, num-3);
+    } else {
+      flower(chosenc1, a+2*num*direction, b-2*num*direction, 30+num + Math.abs(Math.sin((x + num/2)/ 6))*num-mouseY*num/windowHeight, x *num/3*direction + 45, num-3);
+      flower(chosenc2,  a+2*num*direction, b-2*num*direction, 10+0.1*num*num + Math.abs(Math.sin((x + num)/ 6))*num-mouseX*num/windowWidth, x *num/3*direction, num);
+    }
+  }
 }
+
+ function mouseClicked(){
+  setup()
+ }
+      
+  
+  // flower('black', 'white', windowWidth / 2, windowHeight / 2, 10, x);
+
--- 1/sketch.js
+++ 1/sketch.js
@@ -1,11 +1,195 @@
-function setup() {
+
+function setup(){
+
   createCanvas(windowWidth, windowHeight);
+  
+  let color1 = "#435443";
+  let color2 = "#aea965";
+  let color3 = "#939c6e";
+  let color4 = "#4d553a";
+  let color5 = "#384e41";
+  let color6 = "#416141";
+  let color7 = '#4b6141';
+  let color8 = '#526e44';
+  background(color5)
+  let gap = 5;
+  strokeWeight(2);
+  for(x=0;x<windowWidth/4;x += 2*gap){
+      stroke(color2);
+      line(x, 0, x, windowHeight);
+      stroke(color3);
+      line(x + gap, 0, x+ gap, windowHeight);
+
+    }
+
+  for(y=0;y<windowHeight;y += 2*gap){
+    stroke(color2);
+    line(windowWidth/4+gap, y, windowWidth/2,y);
+    stroke(color3);
+    line(windowWidth/4+gap, y+gap, windowWidth/2,y+gap);
+  }
+
+  noStroke()
+  fill(color7)
+  triangle(windowWidth/2,0,windowWidth*3/4,0,windowWidth/2,windowWidth/4)
+  
+  fill(color8)
+  let c = 0
+  for(x=0;x<100;x+=10){
+    triangle(3*windowWidth/4,0,3*windowWidth/4-windowWidth/12-c,3*windowWidth/12-0.9*c,windowWidth/2,0);
+    c = x+10
+  }
+  fill(color4)
+  let b = 0
+  for(x=0;x<100;x+=10){
+    triangle(3*windowWidth/4,0,3*windowWidth/4-windowWidth/12-b,2*windowWidth/12-0.9*b,windowWidth/2,0);
+    b = x+10
+  }
+  fill(color5)
+  let a = 0
+  for(x=0;x<100;x+=10){
+    triangle(3*windowWidth/4,0,3*windowWidth/4-windowWidth/12-1.8*a,windowWidth/12-0.8*a,windowWidth/2,0);
+    a = x+10
+  }
+
+
+  noStroke()
+  fill(color7)
+  triangle(windowWidth,0,windowWidth*3/4,0,windowWidth,windowWidth/4)
+  fill(color8)
+  c=0
+  for(x=0;x<100;x+=10){
+    c = x+10  
+    triangle(windowWidth,0,windowWidth*13/16+c,windowWidth/12-0.8*c,windowWidth,windowWidth/4);
+
+    
+  }
+ 
+  fill(color4)
+  b=0
+  for(x=0;x<100;x+=10){
+    b = x+10  
+    triangle(windowWidth,0,windowWidth*14/16+1.2*b,2*windowWidth/12-b,windowWidth,windowWidth/4);
+
+  }
+  fill(color5)
+  a=0
+  for(x=0;x<100;x+=10){
+    a = x+10  
+    triangle(windowWidth,0,windowWidth*15/16+a,3*windowWidth/12-a,windowWidth,windowWidth/4);
+
+  }
+
+  fill(color4)
+
+  quad(windowWidth/2, windowWidth/4,windowWidth/2, windowHeight,windowWidth*3/4,windowHeight,windowWidth*3/4,0)
+  
+
+  
+  let r = 2
+  for(y=0;y<windowHeight+windowWidth/4;y += 2*gap){
+    fill(color2);
+    for(x = 0;x<(2^0.5)*(windowWidth/8);x+=r){
+      ellipse(windowWidth/2+x, -y+windowWidth/4+windowHeight-x, r)
+    }
+  }
+  
+
+  for(y=0;y<2*windowHeight;y += 2*gap){
+    fill(color2);
+    for(x = 0;x<(2^0.5)*(windowWidth/8);x+=r){
+      ellipse(windowWidth*3/4+x, -windowWidth/4+y+x, r)
+    }
+  }
+  
+
+
+}
+function draw() {
+
 }
 
-function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
-}
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   fill('black')
+//   stroke(0.1)
+//   let colorRange = [
+//     color(252, 244, 3, 90), 
+//     color(255, 28, 28, 90), 
+//     color(3, 36, 252, 90), 
+//     color(42, 156, 61, 90)
+//   ];
+//   for(x = 0;x<200;x++){
+//     let rncolor = random(colorRange);
+//     stroke(rncolor)
+//     let a = random(0, windowWidth+1);
+//     let b = random(0, windowHeight+1);
+//     let c = random(0, windowWidth+1);
+//     let d = random(0, windowHeight+1);
+
+  
+//     line(a, 0, 0, b)
+//     line(c,windowHeight,windowWidth,d)
+//     for(y = 0;y<1;y++){
+//       rncolor = random(colorRange);
+//       stroke(rncolor)
+//       line(c,0,a,windowHeight)
+//       line(0,d,windowWidth,b)
+//     }
+
+//   }
+
+// }
+
+// function draw() {
+
+// }
+// function mouseClicked(){
+//   setup()
+// }
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   noFill();
+//   strokeWeight(1); // Set stroke weight for better visibility
+
+//   let colorRange = [
+//     color(252, 244, 3, 90), 
+//     color(255, 28, 28, 90), 
+//     color(3, 36, 252, 90), 
+//     color(42, 156, 61, 90)
+//   ];
+
+//   for (let x = 0; x < 300; x++) {
+//     let rncolor = random(colorRange);
+//     stroke(rncolor);
+
+//     let [x1, y1] = getRandomEdgePoint();
+//     let [x2, y2] = getRandomEdgePoint();
+
+//     let cx1 = random(0, windowWidth);
+//     let cy1 = random(0, windowHeight);
+//     let cx2 = random(0, windowWidth);
+//     let cy2 = random(0, windowHeight);
+
+//     bezier(x1, y1, cx1, cy1, cx2, cy2, x2, y2);
+//   }
+// }
+
+// function getRandomEdgePoint() {
+//   let edge = floor(random(4)); // Randomly choose an edge: 0 = top, 1 = right, 2 = bottom, 3 = left
+//   switch (edge) {
+//     case 0: // Top edge
+//       return [random(0, windowWidth), 0];
+//     case 1: // Right edge
+//       return [windowWidth, random(0, windowHeight)];
+//     case 2: // Bottom edge
+//       return [random(0, windowWidth), windowHeight];
+//     case 3: // Left edge
+//       return [0, random(0, windowHeight)];
+//   }
+// }
+
+
+
+
